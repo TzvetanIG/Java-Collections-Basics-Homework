559a0f7e991194cd41b08edccbc719e7738b589b
TzvetanIG <cecoig@mail.bg>
2014-05-23 10:58:28 +0300

    Read Url of first level

--- src/_13_WebCrawler.java
+++ src/_13_WebCrawler.java
@@ -1,17 +1,34 @@
 import java.net.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.io.*;
 
 public class _13_WebCrawler {
 
 	public static void main(String[] args) throws Exception {
 
-		URL oracle = new URL("http://www.oracle.com/");
-		BufferedReader in = new BufferedReader(new InputStreamReader(
-				oracle.openStream()));
+		String urlAdress = "http://bg.wikipedia.org";
+		URL url = new URL(urlAdress);
+		BufferedReader input = new BufferedReader(new InputStreamReader(
+				url.openStream()));
 
 		String inputLine;
-		while ((inputLine = in.readLine()) != null)
-			System.out.println(inputLine);
-		in.close();
+		StringBuilder htmlCode = new StringBuilder();
+		while ((inputLine = input.readLine()) != null) {
+			htmlCode.append(inputLine+"\n");
+		}
+			
+		
+//		Pattern pattern = Pattern.compile("<\\s*a[\\s]+[^>]*href[ ]*=\"[^\"]+");
+		Pattern pattern = Pattern.compile("<\\s*a[^>]*>");
+		
+		Matcher matcher = pattern.matcher(htmlCode.toString());
+		
+		while (matcher.find()) {
+			
+		    System.out.println(matcher.group());
+		}
+
 	}
+	
 }
